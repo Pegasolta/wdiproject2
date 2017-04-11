a6f444ee00e085ce6d563ad120d88d40fb67ec53
Felix Tan <felixtanhm@gmail.com>
2017-04-11 09:45:53 +0800

    first push

---
+++ app.js
@@ -0,0 +1,23 @@
+// express set up
+var express = require('express')
+var app = express()
+var port = process.env.PORT || 4000
+
+// mongoose setup
+var dbURI = process.env.PROD_MONGODB || 'mongodb://localhost:27017/wdi-project2'
+var mongoose = require('mongoose')
+mongoose.connect(dbURI)
+
+// check if our connection is okay
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function () {
+  // we're connected!
+  console.log('really really connected')
+})
+
+
+
+app.listen(port, function () {
+  console.log('app is running at ' + port)
+})
